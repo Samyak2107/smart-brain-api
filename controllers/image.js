b0c23e4632ccc7b4f06820a127440a2c821626e6
Samyak2107 <66407436+Samyak2107@users.noreply.github.com>
2020-08-16 21:31:33 +0530

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '362da310774644a18b585a919bf28c82'
+});
+
+const handleApiCall = (req, res) => {
+	app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('Unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('Unable to get count'))
+}
+
+module.exports = {
+	handleImage: handleImage
+}
